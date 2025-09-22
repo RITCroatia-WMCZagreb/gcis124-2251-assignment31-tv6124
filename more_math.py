5ff2e11ba4ead1c77b4b22c32ce43b366539675a
“tv6124” <tv6124@rit.edu>
2025-09-22 19:36:08 +0200

    commiting the assingment

--- more_math.py
+++ more_math.py
@@ -4,10 +4,29 @@
 @ASSESSME.DESCRIPTION: Assignment 3.1
 @ASSESSME.ANALYZE: YES
 '''
+import math 
+def fact(x):
+    if x>0:
+        return math.factorial(x)
+    else:
+        return 0
 
-
+def root(x):
+    if x>0:
+        return math.sqrt(x)
+    else:
+        return 0
+    
+def trunk(x):
+    return math.trunc(x)
+    
 def main():
-   pass
+   number=float(input("Enter a numeric value: "))
+   print(number, "factorial = ", fact(int(number)))
+   print("The square root of ", number, "=", root(number))
+   number_2=float(input("Enter a numeric value: "))
+   print(number_2, "truncated = ", trunk(number_2))
+
 
 
 if __name__ == "__main__":
